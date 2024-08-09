463a464f164df359c5ddcfe536ccba650c4acbd9
gabriel-lumertz <glumertz10@gmail.com>
2024-08-08 22:39:54 -0300

    Mundo 2 exercício 42

--- source/mundo-2/02-condicoes(if-elif)/exercicio-42.py
+++ source/mundo-2/02-condicoes(if-elif)/exercicio-42.py
@@ -11,6 +11,12 @@ lado2 = float(input('Segundo lado: '))
 lado3 = float(input('Terceiro lado: '))
 
 if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2:
-    print('Os segmentos acima PODEM formar um triângulo!')
+    print('Os segmentos acima PODEM formar um triângulo! ', end='')
+    if lado1 == lado2 == lado3:
+        print('Equilátero.')
+    elif lado1 != lado2 != lado3 != lado1:
+        print('Escaleno.')
+    else:
+        print('Isósceles.')
 else:
     print('Os segmentos acima NÃO PODEM formar um triângulo!')
