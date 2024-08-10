eed184eb014f5b86fdbde53572039bb49e20f473
gabriel-lumertz <glumertz10@gmail.com>
2024-08-09 23:51:22 -0300

    Mundo 2 exercício 43

--- source/mundo-2/02-condicoes(if-elif)/exercicio-43.py
+++ source/mundo-2/02-condicoes(if-elif)/exercicio-43.py
@@ -1,7 +1,31 @@
-# Dasanvolva uma lógica qua leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, da acordo com a tabala abaixo:
+# Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
 
-# Abaixo da 18.5: Abaixo do peso
-# Entre 18.5 e 25: Peso ideal
-# Entre 25 e 30: Sobrepeso
-# Entre 30 e 40: Obesidade
-# Acima de 40: Obesidade mórbida
+# – IMC abaixo de 18,5: Abaixo do Peso
+
+# – Entre 18,5 e 25: Peso Ideal
+
+# – 25 até 30: Sobrepeso
+
+# – 30 até 40: Obesidade
+
+# – Acima de 40: Obesidade Mórbida
+
+peso = int(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('O IMS dessa pessoa é de {:.1f} \n'.format(imc))
+    print('Você está abaixo do peso.')
+elif imc <= 25:
+    print('O IMS dessa pessoa é de {:.1f} \n'.format(imc), end='')
+    print('Você está abaixo no peso ideal.')
+elif imc <= 30:
+    print('O IMS dessa pessoa é de {:.1f} \n'.format(imc), end='')
+    print('Você está com sobrepeso.')
+elif imc <= 40:
+    print('O IMS dessa pessoa é de {:.1f} \n'.format(imc), end='')
+    print('Você está abaixo com obesidade.')
+else:
+    print('O IMS dessa pessoa é de {:.1f} \n'.format(imc), end='')
+    print('Você está abaixo com obesidade mórbida.')
