86a0a9442e21b7576d22d1d67948940b7385a079
gabriel-lumertz <glumertz10@gmail.com>
2024-08-10 00:28:30 -0300

    Mundo 2 exercício 44

--- source/mundo-2/02-condicoes(if-elif)/exercicio-44.py
+++ source/mundo-2/02-condicoes(if-elif)/exercicio-44.py
@@ -1,6 +1,45 @@
-# Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamanto:
+# Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
 
-# À vista dinheiro ou cheque: 10% de desconto.
-# À vista no cartão: 5% de desconto.
-# Em até 2x no cartão: preço normal
-# 3x ou mais no cartão: 20% de juros.
+# – à vista dinheiro/cheque: 10% de desconto
+
+# – à vista no cartão: 5% de desconto
+
+# – em até 2x no cartão: preço normal 
+
+# – 3x ou mais no cartão: 20% de juros
+
+print('{:=^40}'.format(' LOJAS GUANABARA '))
+
+print('\n')
+
+preco = float(input('Valor das compras: '))
+
+print('\n')
+
+print('FORMAS DE PAGAMENTO')
+print('[ 1 ] à vista dinheiro ou cheque')
+print('[ 2 ] à vista cartão')
+print('[ 3 ] 2x no cartão')
+print('[ 4 ] 3x ou mais no cartão ')
+
+print('\n')
+
+opcao = int(input('Qual é a opção? '))
+
+if opcao == 4:
+    parcelas = int(input('Quantas parcelas? '))
+
+print('\n')
+
+if opcao == 1:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'. format(preco, preco * 0.9))
+elif opcao == 2:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'. format(preco, preco * 0.95))
+elif opcao == 3:
+    print('Sua compra será parcelada em 2x de R${:.2f} sem juros.'.format(preco / 2))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'. format(preco, preco))
+elif opcao == 4:
+    print('Sua compra será parcelada em {}x de R${:.2f} com juros.'.format(parcelas, preco * 1.2 / parcelas))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'. format(preco, preco * 1.2))
+else:
+    print('Opção inválida.')
