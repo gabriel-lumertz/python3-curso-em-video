a449818c06465eea9368146338a659c581fb7b8b
gabriel-lumertz <glumertz10@gmail.com>
2024-08-11 23:05:33 -0300

    Mundo 2 aula 13

---
+++ source/mundo-2/03-repeticoes(for)/aula13.py
@@ -0,0 +1,26 @@
+# n = int(input('Digite um número: '))
+
+# for c in range(0, n+1):
+#     print(c)
+# print('Fim')
+
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Passo: '))
+
+# for i in range(i, f+1, p):
+#     print(i)
+# print('FIm')
+
+# for c in range(0, 3):
+#     n = int(input('Digite um valor: '))
+
+# print('Fim')
+
+s = 0
+
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
