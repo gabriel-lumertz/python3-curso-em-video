0ba60edff08ad7c12b127cf0d76a3bdc5c801abb
gabriel-lumertz <glumertz10@gmail.com>
2024-08-14 22:29:17 -0300

    Mundo 2 exercício 47

--- source/mundo-2/03-repeticoes(for)/exercicio-47.py
+++ source/mundo-2/03-repeticoes(for)/exercicio-47.py
@@ -1,6 +1,14 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
+
+# Solução 1
 for i in range(1, 51):
     n = i % 2
     if n == 0:
-        print(i)
+        print(i, end = ' ')
+
+print('\n')
+
+# Solução 2
+for i in range(2, 51, 2):
+    print(i, end = ' ')
