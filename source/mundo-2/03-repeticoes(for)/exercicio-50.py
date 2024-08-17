4c4f6f8e3958422704e4bcada14107349311acc5
gabriel-lumertz <glumertz10@gmail.com>
2024-08-16 23:36:28 -0300

    Mundo 2 exercício 50

--- source/mundo-2/03-repeticoes(for)/exercicio-50.py
+++ source/mundo-2/03-repeticoes(for)/exercicio-50.py
@@ -1,11 +1,23 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
-numeros = []
-soma = []
+# numeros = []
+# soma = []
+
+# for i in range(1, 7):
+#     numeros = (int(input('Digite um número inteiro: ')))
+#     if(numeros % 2 == 0):
+#         soma.append(numeros)
+
+# print(f'O valor dos números é: {sum(soma)}.')
+
+
+valores = 0
+contagem = 0
 
 for i in range(1, 7):
-    numeros = (int(input('Digite um número inteiro: ')))
-    if(numeros % 2 == 0):
-        soma.append(numeros)
+    numero = int(input(f'Digite o valor {i}: '))
+    if( numero % 2 == 0):
+        valores += numero
+        contagem += 1
 
-print(f'O valor dos números é: {sum(soma)}.')
+print(f'Você informou {contagem} números PARES e a SOMA foi {valores}.')
