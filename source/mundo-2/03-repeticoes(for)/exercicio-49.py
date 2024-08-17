f6d06a4cfa95715474c1c8d71d31cb4d7aa7e9b7
gabriel-lumertz <glumertz10@gmail.com>
2024-08-16 22:45:44 -0300

    Mundo 2 exercício 49

---
+++ source/mundo-2/03-repeticoes(for)/exercicio-49.py
@@ -0,0 +1,13 @@
+# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+def calucularTabuada(numero):
+    for i in range(1, 11):
+        print(f'{i} X {numero} = {i * numero}')
+
+numero = int(input('Digite um número de 1 a 10: '))
+
+while(0 > numero or numero > 10):
+    print('Número inválido \n')
+    numero = int(input('Digite um número de 1 a 10: '))
+else:
+    calucularTabuada(numero)
