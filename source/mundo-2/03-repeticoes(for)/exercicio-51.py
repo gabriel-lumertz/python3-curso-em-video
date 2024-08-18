c4deba7ec3223450cb1a811dffc9ca178b9269f9
gabriel-lumertz <glumertz10@gmail.com>
2024-08-17 22:25:04 -0300

    Mundo 2 exercício 51

--- source/mundo-2/03-repeticoes(for)/exercicio-51.py
+++ source/mundo-2/03-repeticoes(for)/exercicio-51.py
@@ -1,9 +1,18 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 
-numero = int(input('Digite um valor: '))
+# numero = int(input('Digite um valor: '))
 
-razao = int(input('Digite um valor: '))
+# razao = int(input('Digite um valor: '))
 
-for i in range(1, 11):
-    numero += razao
-    print(numero)
+# for i in range(1, 11):
+#     numero += razao
+#     print(numero)
+
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+
+for i in range(primeiro, decimo + razao, razao):
+    print('{}'.format(i), end=' => ')
+print('Acabou')
